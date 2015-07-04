2a50fe6cd3f4315403b6a7932dcb6e94b9f62c2c
slior <lior.schejter@gmail.com>
2015-07-04 23:06:44 +0300

    move functions into a single namespace - LitJS

--- js/literate.js
+++ js/literate.js
@@ -2,37 +2,57 @@
 var LitJS = {
 	eval : function()
 	{
-		evalScriptBlocks();
-		evalInline();
+		this.evalScriptBlocks();
+		this.evalInline();
 	},
-	lastID : 0
-}
+	evalScriptBlocks : function(jq,_doc)
+	{
+		var $ = jq || jQuery
+		var doc = _doc || document
+		
+		var codeBlocks = $('pre code')
+		var snippets = []
+		for (var i = 0; i < codeBlocks.length; i++) 
+		{
+			var lcb = new LitCodeBlock(codeBlocks[i])
+			//lcb.insertJSTo(doc)
+			snippets.push(lcb.code)
+			if (lcb.title)
+				this.wrapInPanel($(codeBlocks[i].parentElement),lcb.title,lcb.isCollapsible)
+		}
+		
+		if (snippets.length > 0) //actually create and insert the JS element. This also executes the script
+		{
+			var js = doc.createElement('script')
+			js.type = "text/javascript"
+			js.text = snippets.join("\n")
+			doc.head.appendChild(js)	
+		}	
+	},
+	evalInline : function(jq)
+	{
+		var $ = jq || jQuery
+		var inlines = $('code.inline')
+		
+		for (var i = 0, l = inlines.length; i < l; i++)
+			inlines[i].innerText = eval(inlines[i].innerText)
+		
+	},
+	wrapInPanel : function (preElement,title,collapsible,_id)
+	{
+		var id = _id || this.generateBlockID()
 
-function evalScriptBlocks(jq,_doc)
-{
-	var $ = jq || jQuery
-	var doc = _doc || document
-	
-	var codeBlocks = $('pre code')
-	var snippets = []
-	for (var i = 0; i < codeBlocks.length; i++) 
+		preElement.wrap("<div class='panel panel-info'><div class='panel-body' id='" + id + "'></div></div>")
+		var bodyDiv = preElement.parent()
+		var collapseLink = collapsible ? " <a href=\"javascript:$('#" + id + "').toggle(); LitJS.toggleCollapseSymbol($('#" + id + "').parent().children('.panel-heading').children())\">[-]</a>" : ""
+		bodyDiv.before("<div class='panel-heading'>" + (title||"Code") + collapseLink + "</div>")
+	},
+	toggleCollapseSymbol : function(el)
 	{
-		var lcb = new LitCodeBlock(codeBlocks[i])
-		//lcb.insertJSTo(doc)
-		snippets.push(lcb.code)
-		if (lcb.title)
-			wrapInPanel($(codeBlocks[i].parentElement),lcb.title,lcb.isCollapsible)
-	}
-	
-	if (snippets.length > 0) //actually create and insert the JS element. This also executes the script
-	{
-		var js = doc.createElement('script')
-		js.type = "text/javascript"
-		js.text = snippets.join("\n")
-		doc.head.appendChild(js)	
-	}
-	
-	
+		el.text(el.text() == '[-]' ? '[+]' : '[-]')
+	},
+	generateBlockID : function() { return "block_" + (LitJS.lastID++); },
+	lastID : 0
 }
 
 
@@ -52,44 +72,6 @@ function LitCodeBlock(htmlCodeNode)
 	}
 	
 	return this;
-}
-
-function wrapInPanel(preElement,title,collapsible,_id)
-{
-	var id = _id || generateBlockID()
-
-	preElement.wrap("<div class='panel panel-info'><div class='panel-body' id='" + id + "'></div></div>")
-	var bodyDiv = preElement.parent()
-	var collapseLink = collapsible ? " <a href=\"javascript:$('#" + id + "').toggle(); toggleCollapseSymbol($('#" + id + "').parent().children('.panel-heading').children())\">[-]</a>" : ""
-	bodyDiv.before("<div class='panel-heading'>" + (title||"Code") + collapseLink + "</div>")
-}
-
-function toggleCollapseSymbol(el)
-{
-	el.text(el.text() == '[-]' ? '[+]' : '[-]')
-}
-
-function generateBlockID()
-{
-	return "block_" + (LitJS.lastID++)
-}
-
-
-function createAndInsertJS(codeBlock,doc)
-{
-	var js = doc.createElement('script');
-	js.innerHTML = codeBlock.innerText;
-	doc.head.appendChild(js);
-}
-
-function evalInline(jq)
-{
-	var $ = jq || jQuery
-	var inlines = $('code.inline')
-	
-	for (var i = 0, l = inlines.length; i < l; i++)
-		inlines[i].innerText = eval(inlines[i].innerText)
-	
 }
 
 
